95d3efc44701ad539d8d54dce92d3e16fff5a2a7
Ben Gao <lamdda@benxgao.com>
2020-09-24 09:24:44 +1200

    Refactor Vue folder structure

--- src/router.ts
+++ src/router.ts
@@ -1,11 +1,7 @@
 import { createRouter, createWebHashHistory } from 'vue-router';
-import Cart from './pages/Schedule.vue';
-import Checkout from './pages/WorkoutPlan.vue';
+import Schedule from './views/Schedule.vue';
 
 export default createRouter({
   history: createWebHashHistory(),
-  routes: [
-    { path: '/', name: 'cart', component: Cart },
-    { path: '/checkout', name: 'checkout', component: Checkout },
-  ],
+  routes: [{ path: '/', name: 'schedule', component: Schedule }],
 });
