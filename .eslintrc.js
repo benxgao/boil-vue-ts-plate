5950506d49d7ee2e73a3c566650b0e2b71daa6c4
Ben Gao <lamdda@benxgao.com>
2020-09-23 15:29:24 +1200

    Config Eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,11 +1,15 @@
+import prettierRules from './.prettierrc.json';
+
 module.exports = {
   env: { jest: true, browser: true },
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/eslint-recommended',
-    // 'plugin:@typescript-eslint/recommended-requiring-type-checking',
     'plugin:import/typescript',
     'plugin:vue/vue3-recommended',
+    'plugin:prettier-vue/recommended',
+    // 'prettier/vue', // Do not add `'prettier/vue'` if you don't want to use prettier for `<template>` blocks
+    // 'plugin:@typescript-eslint/recommended-requiring-type-checking',
   ],
   plugins: ['@typescript-eslint', 'jest', 'prettier'],
   parser: 'vue-eslint-parser',
@@ -18,17 +22,68 @@ module.exports = {
   },
   settings: {
     'import/extensions': ['.ts'],
+    'prettier-vue': {
+      // Settings for how to process Vue SFC Blocks
+      SFCBlocks: {
+        /**
+         * Use prettier to process `<template>` blocks or not
+         *
+         * If set to `false`, remember not to `extends: ['prettier/vue']`, as you need the rules from `eslint-plugin-vue` to lint `<template>` blocks
+         *
+         * @default true
+         */
+        template: true,
+
+        /**
+         * Use prettier to process `<script>` blocks or not
+         *
+         * @default true
+         */
+        script: true,
+
+        /**
+         * Use prettier to process `<style>` blocks or not
+         *
+         * @default true
+         */
+        style: true,
+
+        // Settings for how to process custom blocks
+        customBlocks: {
+          // Treat the `<docs>` block as a `.markdown` file
+          docs: { lang: 'markdown' },
+
+          // Treat the `<config>` block as a `.json` file
+          config: { lang: 'json' },
+
+          // Treat the `<module>` block as a `.ts` file
+          module: { lang: 'ts' },
+
+          // Ignore `<comments>` block (omit it or set it to `false` to ignore the block)
+          comments: false,
+
+          // Other custom blocks that are not listed here will be ignored
+        },
+      },
+
+      // Use prettierrc for prettier options or not (default: `true`)
+      usePrettierrc: false,
+
+      // Set the options for `prettier.getFileInfo`.
+      // @see https://prettier.io/docs/en/api.html#prettiergetfileinfofilepath-options
+      fileInfoOptions: {
+        // Path to ignore file (default: `'.prettierignore'`)
+        // Notice that the ignore file is only used for this plugin
+        ignorePath: '.testignore',
+
+        // Process the files in `node_modules` or not (default: `false`)
+        withNodeModules: false,
+      },
+    },
   },
   rules: {
-    'prettier/prettier': [
-      'error',
-      {
-        arrowParens: 'always',
-        printWidth: 100,
-        singleQuote: true,
-        trailingComma: 'es5',
-        // plugins: ['prettier-plugin-package']
-      },
-    ],
+    'prettier/prettier': ['error', prettierRules],
+    'prettier-vue/prettier': ['error', prettierRules],
+    // 'vue/no-multiple-template-root': 'off',
   },
 };
