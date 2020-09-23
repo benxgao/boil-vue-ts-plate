382f45bebe42dbeb5b147db98099e5418fdbcad0
Ben Gao <lamdda@benxgao.com>
2020-09-23 19:50:44 +1200

    Config Tailwind

--- babel.config.js
+++ babel.config.js
@@ -8,7 +8,16 @@ module.exports = {
         },
       },
     ],
+    [
+      '@vue/app',
+      {
+        polyfills: ['es.promise', 'es.symbol'],
+      },
+    ],
     '@babel/preset-typescript',
   ],
-  plugins: ['@babel/proposal-class-properties', '@babel/plugin-proposal-object-rest-spread'],
+  plugins: [
+    '@babel/proposal-class-properties',
+    '@babel/plugin-proposal-object-rest-spread',
+  ],
 };
--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,18 @@
 <template>
+  <Nav class="mb-6" />
   <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3.0 + Vite" />
+  <HelloWorld msg="Welcome to Your Vue.js App" class="text-center" />
+  <div class="container mx-auto py-10"></div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from './components/HelloWorld.vue';
+import Nav from './components/Nav.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
-}
+    HelloWorld,
+  },
+};
 </script>
