80253c9576ee69cf6e30348f1440e876004018ed
Ben Gao <lamdda@benxgao.com>
2020-09-23 19:01:40 +1200

    Add webpack

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,8 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import './index.css'
+import { createApp } from 'vue';
+import { router } from './router';
+import { store } from './store';
+import App from './App.vue';
 
-createApp(App).mount('#app')
+import './index.css';
+
+createApp(App).use(router).use(store).mount('#app');
